9866ae42e6286bee2e056342e6504c2ac1326f02
Eric Mugo Thiong'o <mugoericpg20@gmail.com>
2023-04-30 16:27:22 +0300

    Added GifSearch

---
+++ src/components/GifSearch.js
@@ -0,0 +1,26 @@
+import React, {useState} from "react";
+
+function GifSearch({ onSearch }){
+    const [query, setQuery] = useState("");
+
+
+    function handleSubmit(e){
+        e.preventDefault();
+        onSearch(query);
+
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            placeholder="Search for gifs!"
+          />
+          <button type="submit">Search</button>
+        </form>
+      );
+}
+
+export default GifSearch;
