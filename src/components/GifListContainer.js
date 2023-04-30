ebb1289b7233729756b0047642196b1cba44a45f
Eric Mugo Thiong'o <mugoericpg20@gmail.com>
2023-04-30 14:47:09 +0300

    Added fetch url

--- src/components/GifListContainer.js
+++ src/components/GifListContainer.js
@@ -2,5 +2,28 @@ import react, {useState} from "react";
 
 
 function GifListContainer(){
-    const [gifs, setGifs] = useState([])
+    const [gifs, setGifs] = useState([]);
+
+    function handleSearch(query){
+        const apiKey = "lyFN4ucs1L9BG50JSb9WSEleE7eCEpuv";
+        const url = `https://api.giphy.com/v1/gifs/search?q=${query}&api_key=${apiKey}&rating=g&limit=3`;
+
+        fetch (url)
+        .then(r => r.json)
+        .then(data => {
+            const gifData = data.data.map((gif) => ({
+                id: gif.id,
+                url: gif.images.original.url
+            }));
+            setGifs(gifData);
+        })
+        .catch(console.error);
+
+    }
+
+    
+
+
+
+
 }
